11b648a94f31bab0a7042183243dc7a75fb5885c
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-09 15:58:11 +0530

    binarystring.C

---
+++ binarystring.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];  int i,count;
+printf("\n enter the string");
+for(i=0;i<=100;i++)
+{
+scanf("%s",a[i]);
+}
+for(i=0;i<100;i++)
+{
+if(a[i]%10==0/a[i]%10==1)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+}
+}
